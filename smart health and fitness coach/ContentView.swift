27540f001c1c8932e832df4e031cc15a59e18e6c
Khushank <khushank.inhouse@gmail.com>
2025-10-23 09:31:25 +0400

    Initial Commit

---
+++ smart health and fitness coach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  smart health and fitness coach
+//
+//  Created by Khushank Rawat on 23/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ smart health and fitness coach/smart_health_and_fitness_coachApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_health_and_fitness_coachApp.swift
+//  smart health and fitness coach
+//
+//  Created by Khushank Rawat on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct smart_health_and_fitness_coachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
